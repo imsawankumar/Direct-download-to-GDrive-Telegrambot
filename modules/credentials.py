683c366731ddc4d98c2647e1167559146166c168
imsawankumar <25695674+imsawankumar@users.noreply.github.com>
2020-10-15 20:32:40 +0530

    Update credentials.py

--- modules/credentials.py
+++ modules/credentials.py
@@ -2,5 +2,5 @@ class Creds(object):
     TOKEN = "1330954432:AAEmrw0mmE36KeD9FkXrS23Vx-aWrSpkCdA"
     CLIENT_ID = "833719774142-i8l86tr2i44tb06r43l4s01esrh6b0va.apps.googleusercontent.com"
     CLIENT_SECRET = "gJvLUe3Rop7wxz4zlfakgSUk"
-    PATH_ID = "folder id of the gdrive folder in which the bot will upload its files"
+    PATH_ID = "10EVfURc6r9Otfb6lhWp7Dh-z8EpDl33D"
     USER_ID = "925648696"
